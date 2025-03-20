5deb34fceb7fb720589698d3b0937d0c3d930d14
Andres Romero <af.romerob1@uniandes.edu.co>
2025-03-20 11:49:59 -0500

    Add order flow views

---
+++ LastBite/LastBite/Views/Cart/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  LastBite
+//
+//  Created by Andrés Romero on 20/03/25.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
---
+++ LastBite/LastBite/Views/Order/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  LastBite
+//
+//  Created by Andrés Romero on 20/03/25.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
